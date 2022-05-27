04be8463a1ccbb6a7302854b51b32379e39fd76d
Bruno Coutinho <brunodba@live.com>
2022-05-27 18:11:58 -0300

    Categorias versus produtos.

--- routes/admin-categories.php
+++ routes/admin-categories.php
@@ -1,22 +1,9 @@
 <?php
 
-use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
 use \Hcode\Model\Category;
-
-$app->get("/categories/:idcategory", function ($idcategory) {
-    $category = new Category();
-
-    $category->get((int)$idcategory);
-
-    $page = new Page();
-
-    $page->setTpl("category", [
-        "category" => $category->getValues(),
-        "products" => []
-    ]);
-});
+use Hcode\Model\Product;
 
 $app->get('/admin/categories', function () {
 
@@ -96,5 +83,58 @@ $app->post("/admin/categories/:idcategory", function ($idcategory) {
     $category->save();
 
     header("Location: /admin/categories");
+    exit;
+});
+
+$app->get('/admin/categories/:idcategory/products', function ($idcategory) {
+
+    User::verifyLogin();
+
+    $category = new Category();
+
+    $category->get((int)$idcategory);
+
+    $page = new PageAdmin();
+
+    $page->setTpl("categories-products", array(
+        "category" => $category->getValues(),
+        'productsRelated' => $category->getProducts(),
+        'productsNotRelated' => $category->getProducts(false)
+    ));
+});
+
+$app->get('/admin/categories/:idcategory/products/:idproduct/add', function ($idcategory, $idproduct) {
+
+    User::verifyLogin();
+
+    $category = new Category();
+
+    $category->get((int)$idcategory);
+
+    $product = new Product();
+
+    $product->get((int)$idproduct);
+
+    $category->addProduct($product);
+
+    header("Location: /admin/categories/" . $idcategory . "/products");
+    exit;
+});
+
+$app->get('/admin/categories/:idcategory/products/:idproduct/remove', function ($idcategory, $idproduct) {
+
+    User::verifyLogin();
+
+    $category = new Category();
+
+    $category->get((int)$idcategory);
+
+    $product = new Product();
+
+    $product->get((int)$idproduct);
+
+    $category->removeProduct($product);
+
+    header("Location: /admin/categories/" . $idcategory . "/products");
     exit;
 });
